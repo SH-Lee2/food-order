84d5547e7f5baa4bcc5099415db7b477f4e15368
lsh <tlgud585@gmail.com>
2022-04-21 18:03:46 +0900

    ADD firebase

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,13 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartItem from "./CartItem";
 
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
+import Checkout from "./Checkout";
 
 const Cart = ({ onClose }) => {
+    const [isCheckout, setIsCheckout] = useState(false);
     const ctx = useContext(CartContext);
 
     const addItemHandler = (item) => {
@@ -15,15 +17,24 @@ const Cart = ({ onClose }) => {
         ctx.removeItem(id);
     };
 
-    const orderHandler = () => {
-        if (ctx.items.length !== 0) {
-            console.log("주문!");
-        } else {
-            console.log("주문 목록이 없습니다.");
-        }
+    const orderHandler = (event) => {
+        event.preventDefault();
+        setIsCheckout(true);
     };
     const totalAmount = ctx.totalAmount.toFixed(2);
     const hasItems = ctx.items.length > 0;
+    const modalActions = (
+        <div className={classes.actions}>
+            <button className={classes["button--alt"]} onClick={onClose}>
+                Close
+            </button>
+            {hasItems && (
+                <button className={classes.button} onClick={orderHandler}>
+                    Order
+                </button>
+            )}
+        </div>
+    );
     return (
         <Modal onClose={onClose}>
             <ul className={classes["cart-items"]}>
@@ -43,16 +54,8 @@ const Cart = ({ onClose }) => {
                 <span>Total Amount</span>
                 <span>${totalAmount}</span>
             </div>
-            <div className={classes.actions}>
-                <button className={classes["button--alt"]} onClick={onClose}>
-                    Close
-                </button>
-                {hasItems && (
-                    <button className={classes.button} onClick={orderHandler}>
-                        Order
-                    </button>
-                )}
-            </div>
+            {isCheckout && <Checkout onClose={onClose} />}
+            {!isCheckout && modalActions}
         </Modal>
     );
 };
